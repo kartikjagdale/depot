2dbcec6c6215abfae3edbcd9884a05f88d217c1b
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-05 17:31:45 +0530

    Completed Iteration 5: Caching of Partial Results but turned of Cahching as in Devlopment Phase

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
+#Author : VoidZero
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format:{
 		with: %r{\.(gif|jpg|png)\Z}i,
-		message: 'mus be a url for GIF, JPG or PNG image.'	
-
+		message: 'must be a url for GIF, JPG or PNG image.'	
 	}
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
