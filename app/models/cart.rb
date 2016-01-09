a35099ad70f3998c4cee390532515d6220a672d4
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-10 04:21:28 +0530

    Chapter 10: Iteration E1: Creating a Smarter Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,9 +6,21 @@
 	Secondly "dependent: :destroy", if cart destroyed we want rails to destroy line item
 	assocaited with that cart too.
 		_VoidZero
+
+	"find_by" method returns an existing line_item or nil
 =end
 
 
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def add_product(product_id)
+		current_item = line_items.find_by(product_id: product_id)
+		if current_item
+			current_item.quantity +=1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+			current_item
+	end
 end
