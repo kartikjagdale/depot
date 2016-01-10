446b6b75aaf80210ae6c0bd74b14b956e0b6f69d
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-10 09:57:52 +0530

    Chapter 11: Iteration F2: Creating a Ajax-Based Cart/ Now items get added to cart without reloading thanks to AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,10 +30,10 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
     
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
