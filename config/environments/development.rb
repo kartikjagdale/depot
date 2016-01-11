b4916cc6a4efe1fe4f28480ff035a90a89180203
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-11 20:02:49 +0530

    Completed: CH:13: Iteration H1: Sending Confirmation Emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :test
+  end
 end
