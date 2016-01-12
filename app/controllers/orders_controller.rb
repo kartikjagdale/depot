99d476202e71f488baae73f0456f7e12cbef1fe4
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-12 17:45:51 +0530

    Translated Storefront and Checkout into Spanish

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
+          I18n.t('.thanks') }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
