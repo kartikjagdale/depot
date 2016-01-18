bd1e0c8c968c41c1cbddbaab5027c1f6f916b094
Kartik Jagdale <kartikjagdale11@gmail.com>
2016-01-18 18:42:26 +0530

    Addded a bit of Foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery.ui.effect-blind
 //= require jquery_ujs
-//= require turbolinks
+//= require foundation
 //= require_tree .
+$(function(){ $(document).foundation(); });
